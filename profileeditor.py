32255bde5cbef31e601b1cc19ecb2eb312bcc664
spartantri <mleosrivas@mastersprogram.sans.edu>
2016-08-25 19:27:07 +0200

    edit locations, arguments, cookies, session cookies and headers individually or in batch

--- profileeditor.py
+++ profileeditor.py
@@ -10,20 +10,21 @@
 # License:     Apache License Version 2.0
 # -------------------------------------------------------------------------------
 import xml.dom.minidom
-from xml.etree.ElementTree import Element, SubElement, Comment, tostring
+from xml.etree.ElementTree import SubElement, tostring
 import xml.etree.ElementTree as ET
 
 input_file = 'test.xml'
 usage = '''Usage:
-    input [file]                   - Change/Display input file
-    print [pretty]                 - Print (Pretty) input file
-    location [add|remove}          - Location List/Add/Remove
-    set [location|method]          - Display/Set current working Location/Method
-    parameter [add|remove]         - Display/add/remove arguments
-    cookie [add|remove]            - Display/add/remove cookies
-    sessioncookie [add|remove]     - Display/add/remove sessioncookies
-    header [add|remove]            - Display/add/remove headers
-    exit                           - Exit'''
+    input [file]                            - Change/Display input file
+    print [pretty]                          - Print (Pretty) input file
+    location [add|remove] [target]          - Location List/Add/Remove
+    set [location|method] [target]          - Display/Set current working Location/Method
+    parameter [add|remove] [target]         - Display/add/remove arguments
+    cookie [add|remove] [target]            - Display/add/remove cookies
+    sessioncookie [add|remove] [target]     - Display/add/remove sessioncookies
+    header [add|remove] [target]            - Display/add/remove headers
+    history [#]                             - Display command history
+    exit                                    - Exit'''
 location = '*'
 method = '*'
 
@@ -74,32 +75,53 @@ def location_print():
     return
 
 
-def location_add():
+def location_add(target=False):
     tree = ET.parse(input_file)
     root = tree.getroot()
     xml_context = root.find('Context')
     xml_location = SubElement(xml_context, 'Resource')
-    resource = raw_input("Enter location : ").split(' ')[0]
-    xml_location.set('name', resource)
-    scheme = raw_input("http or https (default) : ").split(' ')[0]
-    xml_scheme = SubElement(xml_location, 'Scheme')
-    if resource.lower() == 'http':
-        xml_scheme.set('value', scheme)
+    if not target:
+        resource = raw_input("Enter location : ").split(' ')[0]
+        scheme = raw_input("http or https (default) : ").split(' ')[0]
+        if scheme.lower() == 'http':
+            scheme = 'http'
+        else:
+            scheme = 'https'
+        print 'Confirm values'
+        print 'Location : %s' % resource
+        print 'Method   : %s' % method
+        print 'Scheme   : %s' % scheme
+        confirmation = str(raw_input("Accept values : Y/N").split(' ')[0]).upper()
+        if confirmation != 'Y':
+            print ' Aborting...'
+            return
     else:
-        xml_scheme.set('value', 'https')
-    #xml_method = SubElement(xml_location, 'Method')
-    #xml_method.set('value', method)
+        resource = str(target).split(' ')[0]
+        scheme = 'https'
+    xml_location.set('name', resource)
+    xml_scheme = SubElement(xml_location, 'Scheme')
+    xml_scheme.set('value', scheme)
     for xml_item in root.iter('Resource'):
         print xml_item.get('name')
     tree.write(input_file)
     return
 
 
-def location_remove():
+def location_remove(target=False):
     tree = ET.parse(input_file)
     root = tree.getroot()
     removed = 0
-    resource = raw_input("Enter location : ").split(' ')[0]
+    if not target:
+        resource = raw_input("Enter location : ").split(' ')[0]
+        print 'Confirm values'
+        print 'Location : %s' % resource
+        print 'Method   : %s' % method
+        confirmation = str(raw_input("Accept values : Y/N").split(' ')[0]).upper()
+        if confirmation != 'Y':
+            print ' Aborting...'
+            return
+    else:
+        resource = str(target).split(' ')[0]
     for xml_item in reversed(xrange(len(root[0]))):
         if root[0][xml_item].get('name') == resource:
             print 'Removing : ', root[0][xml_item].get('name')
@@ -107,7 +129,7 @@ def location_remove():
             removed += 1
     if removed > 0:
         tree.write(input_file)
-        print 'Removed %d locations' % (removed)
+        print 'Removed %d locations' % removed
     else:
         print 'Not found'
     return
@@ -115,9 +137,9 @@ def location_remove():
 
 def print_warning(element):
     if location == '*':
-        print 'Location set to * will print the %s from all locations in the profile' % (element)
+        print 'Location set to * will print the %s from all locations in the profile' % element
     if method == '*':
-        print 'Method set to * will print the %s from all methods' % (element)
+        print 'Method set to * will print the %s from all methods' % element
     return
 
 
@@ -125,7 +147,7 @@ def element_print(element):
     global location, method
     tree = ET.parse(input_file)
     root = tree.getroot()
-    print '%s check:' % (str(element).capitalize())
+    print '%s check:' % element
     print_warning(element)
     for xml_location in root.findall('Context/Resource'):
         if xml_location.get('name') == location or location == '*':
@@ -134,20 +156,36 @@ def element_print(element):
                     if xml_method.get('value') == method or method == '*':
                         print xml_location.get('name')
                         print ' ', xml_method.tag, xml_method.get('value')
-                        for x in xml_method.findall(str(element).capitalize()):
+                        for x in xml_method.findall(element):
                             print '   ', x.tag, x.attrib
     return
 
 
-def element_add(element):
+def element_add(element, target=False, regex='any'):
     global location, method
     tree = ET.parse(input_file)
     root = tree.getroot()
     elements_added = 0
-    print 'Adding %s :' % (str(element).capitalize())
+    print 'Adding %s :' % element
     print_warning(element)
-    arg = raw_input("Enter %s name : " % (element)).split(' ')[0]
-    regex = raw_input("Enter regex : ").split(' ')[0]
+    if not target:
+        arg = raw_input("Enter %s name : " % element).split(' ')[0]
+        regex = raw_input("Enter regex [any] : ").split(' ')[0]
+        if regex == 'any' or regex == '':
+            regex == '.*'
+        print 'Confirm values'
+        print 'Location : %s' % location
+        print 'Method   : %s' % method
+        print '%s : %s' % (element, arg)
+        print 'Regex    : %s' % regex
+        confirmation = str(raw_input("Accept values : Y/N ").split(' ')[0]).upper()
+        if confirmation != 'Y':
+            print ' Aborting...'
+            return
+    else:
+        arg = target
+        if regex == 'any':
+            regex = '.*'
     for xml_location in root.findall('Context/Resource'):
         if xml_location.get('name') == location or location == '*':
             for xml_method in xml_location:
@@ -167,29 +205,38 @@ def element_add(element):
     return
 
 
-def element_remove(element):
+def element_remove(element, target=False, regex='any'):
     global location, method
     tree = ET.parse(input_file)
     root = tree.getroot()
     elements_removed = 0
-    print 'Adding %s :' % (str(element).capitalize())
+    print 'Removing %s :' % element
     print_warning(element)
-    arg = raw_input("Enter %s name : " % (element)).split(' ')[0]
-    regex = raw_input("Enter regex [any]: ").split(' ')[0]
+    if not target:
+        arg = raw_input("Enter %s name : " % element).split(' ')[0]
+        regex = raw_input("Enter regex [any] : ").split(' ')[0]
+        if regex == '':
+            regex = 'any'
+        print 'Confirm values'
+        print 'Location : %s' % location
+        print 'Method   : %s' % method
+        print '%s : %s' % (element, arg)
+        print 'Regex    : %s' % regex
+        confirmation = str(raw_input("Accept values : Y/N ").split(' ')[0]).upper()
+        if confirmation != 'Y':
+            print ' Aborting...'
+            return
+    else:
+        arg = target
     for xml_location in root.findall('Context/Resource'):
         if xml_location.get('name') == location or location == '*':
             for xml_method in xml_location:
                 if xml_method.tag == 'Method':
                     if xml_method.get('value') == method or method == '*':
                         for deleteme in xml_method.findall(element):
-                            if deleteme.get('name') == arg:
+                            if deleteme.get('name') == arg and (deleteme.get('regexp') == regex or regex == 'any'):
                                 xml_method.remove(deleteme)
                                 elements_removed += 1
-
-                        # print tree.getroot().getchildren(), xml_method.getchildren()
-                        #print xml_method.getparent().index(xml_method)
-                        #elements_removed += 1
-
     if elements_removed > 0:
         print 'Removed %d %s' % (elements_removed, element)
     else:
@@ -198,60 +245,109 @@ def element_remove(element):
     return
 
 
-def main():
+def interactive_menu():
     global input_file, location, method
     options = {'parameter': 'Parameter', 'cookie': 'Cookie', 'sessioncookie': 'SessionCookie', 'header': 'Header'}
+    command_history = []
     while True:
         selected_option = raw_input("Enter action [help] : ").split(' ')
-        if selected_option[0].lower() == 'help':
+        selected_qty = len(selected_option)
+        command = False
+        action = False
+        target = False
+        regex = 'any'
+        if selected_qty > 0:
+            command = selected_option[0].lower()
+            action = False
+            target = False
+        if selected_qty > 1:
+            action = selected_option[1]
+            target = False
+        if selected_qty > 2:
+            target = selected_option[2]
+        if selected_qty > 3:
+            regex = selected_option[3]
+        if command != 'history':
+            command_history.append(selected_option)
+        if command == 'help':
             print usage
-        elif selected_option[0].lower() == 'input':
-            if len(selected_option) > 1:
+        elif command == 'input':
+            if not action:
+                print 'Current input file : %s' % input_file
                 input_file = raw_input("Input file : ")
-            print 'Current input file: %s' % (input_file)
-        elif selected_option[0].lower() == 'print':
-            if len(selected_option) > 1 :
-                if selected_option[1].lower() == 'pretty':
-                    print transform_xml(input_file)
+                # check if file exist
+            else:
+                print 'Changed input file to : %s' % input_file
+        elif command == 'print':
+            if action == 'pretty':
+                print transform_xml(input_file)
             else:
                 with open(input_file) as f:
                     for line in f:
                         print line,
                 print ''
-        elif selected_option[0].lower() == 'set':
-            if len(selected_option) == 1:
+        elif command == 'set':
+            if not action:
                 set_print()
             else:
-                if selected_option[1].lower() == 'location':
-                    if len(selected_option) > 2:
-                        set_location(selected_option[2])
+                if action == 'location':
+                    if target:
+                        set_location(target)
                     else:
                         set_location()
-                elif selected_option[1].lower() == 'method':
-                    if len(selected_option) > 2:
-                        set_method(str(selected_option[2]).upper())
+                elif action == 'method':
+                    if target:
+                        set_method(str(target).upper())
                     else:
                         set_method()
-        elif selected_option[0].lower() == 'location':
-            if len(selected_option) == 1:
+        elif command == 'location':
+            if not action:
                 location_print()
             else:
-                if selected_option[1].lower() == 'add':
-                    location_add()
-                elif selected_option[1].lower() == 'remove':
-                    location_remove()
-        elif selected_option[0].lower() in ['parameter', 'cookie', 'sessioncookie', 'header']:
-            if len(selected_option) == 1:
-                element_print(options[selected_option[0].lower()])
+                if action == 'add':
+                    if target:
+                        location_add(target)
+                    else:
+                        location_add()
+                elif action == 'remove':
+                    if target:
+                        location_remove(target)
+                    else:
+                        location_remove()
+        elif command in ['parameter', 'cookie', 'sessioncookie', 'header']:
+            if not action:
+                element_print(options[command])
             else:
-                if selected_option[1].lower() == 'add':
-                    element_add(options[selected_option[0].lower()])
-                elif selected_option[1].lower() == 'remove':
-                    element_remove(options[selected_option[0].lower()])
-        elif selected_option[0].lower() == 'exit':
+                if action == 'add':
+                    if target:
+                        element_add(options[command], target, regex)
+                    else:
+                        element_add(options[command])
+                elif action == 'remove':
+                    if target:
+                        element_remove(options[command], target, regex)
+                    else:
+                        element_remove(options[command])
+        elif command == 'history':
+            if not action:
+                for idx, line in enumerate(command_history):
+                    print idx, ':', ' '.join(line)
+            else:
+                if action.isdigit():
+                    cmd_list = len(command_history)
+                    cmd_show = int(action)
+                    if cmd_show >= cmd_list:
+                        cmd_show = cmd_list
+                    for idx, line in enumerate(command_history[-cmd_show:]):
+                        print cmd_list+idx-cmd_show, ':', ' '.join(line)
+        elif command == 'exit':
             exit()
 
 
+def main():
+    pass
+
+
 if __name__ == '__main__':
-    main()
+    interactive_menu()
 
